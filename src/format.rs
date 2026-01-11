richardscollin/tmux-rs

commit 64684b08869c6de0199ebd83be04c7d42d89cce5

refactor: reimplement bitstr safely

src/format.rs
@@ -970,7 +970,7 @@ pub unsafe fn format_cb_pane_tabs(ft: *mut format_tree) -> format_table_type {
 
         let mut first = true;
         for i in 0..(*(*wp).base.grid).sx {
-            if !bit_test((*wp).base.tabs, i) {
+            if !(*wp).base.tabs.as_ref().unwrap().bit_test(i) {
                 continue;
             }
 

src/input.rs
@@ -1405,7 +1405,7 @@ unsafe fn input_c0_dispatch(ictx: *mut input_ctx) -> i32 {
                     // Don't tab beyond the end of the line.
                     // Find the next tab point, or use the last column if none.
                     (*s).cx += 1;
-                    if bit_test((*s).tabs, (*s).cx) {
+                    if (*s).tabs.as_ref().unwrap().bit_test((*s).cx) {
                         break;
                     }
                 }
@@ -1468,7 +1468,7 @@ unsafe fn input_esc_dispatch(ictx: *mut input_ctx) -> i32 {
             }
             Ok(input_esc_type::INPUT_ESC_HTS) => {
                 if (*s).cx < screen_size_x(s) {
-                    bit_set((*s).tabs, (*s).cx);
+                    (*s).tabs.as_mut().unwrap().bit_set((*s).cx);
                 }
             }
             Ok(input_esc_type::INPUT_ESC_RI) => {
@@ -1543,7 +1543,7 @@ unsafe fn input_csi_dispatch(ictx: *mut input_ctx) -> i32 {
                         n -= 1;
                         loop {
                             cx -= 1;
-                            if cx == 0 || bit_test((*s).tabs, cx) {
+                            if cx == 0 || (*s).tabs.as_ref().unwrap().bit_test(cx) {
                                 break;
                             }
                         }
@@ -1760,10 +1760,10 @@ unsafe fn input_csi_dispatch(ictx: *mut input_ctx) -> i32 {
                 -1 => (),
                 0 => {
                     if (*s).cx < screen_size_x(s) {
-                        bit_clear((*s).tabs, (*s).cx);
+                        (*s).tabs.as_mut().unwrap().bit_clear((*s).cx);
                     }
                 }
-                3 => bit_nclear((*s).tabs, 0, screen_size_x(s) - 1),
+                3 => (*s).tabs.as_mut().unwrap().bit_nclear(0, screen_size_x(s) - 1),
                 _ => log_debug!("{}: unknown '{}'", __func__, (*ictx).ch as u8 as char),
             },
             Ok(input_csi_type::INPUT_CSI_VPA) => {

src/lib.rs
@@ -1041,7 +1041,6 @@ enum screen_cursor_style {
 
 /// Virtual screen.
 #[repr(C)]
-#[derive(Clone)]
 struct screen {
     title: *mut u8,
     path: *mut u8,
@@ -1077,7 +1076,7 @@ struct screen {
     saved_cell: grid_cell,
     saved_flags: i32,
 
-    tabs: *mut bitstr_t,
+    tabs: Option<BitStr>,
     sel: *mut screen_sel,
 
     #[cfg(feature = "sixel")]

src/screen_.rs
@@ -74,7 +74,7 @@ pub unsafe fn screen_init(s: *mut screen, sx: u32, sy: u32, hlimit: u32) {
         (*s).default_mode = mode_flag::empty();
         (*s).ccolour = -1;
         (*s).default_ccolour = -1;
-        (*s).tabs = null_mut();
+        (*s).tabs = None;
         (*s).sel = null_mut();
 
         #[cfg(feature = "sixel")]
@@ -138,7 +138,7 @@ pub unsafe fn screen_reset_hyperlinks(s: *mut screen) {
 pub unsafe fn screen_free(s: *mut screen) {
     unsafe {
         free_((*s).sel);
-        free_((*s).tabs);
+        (*s).tabs = None;
         free_((*s).path);
         free_((*s).title);
 
@@ -164,16 +164,11 @@ pub unsafe fn screen_free(s: *mut screen) {
 /// Reset tabs to default, eight spaces apart.
 pub unsafe fn screen_reset_tabs(s: *mut screen) {
     unsafe {
-        free_((*s).tabs);
-
-        (*s).tabs = bit_alloc(screen_size_x(s));
-        if (*s).tabs.is_null() {
-            fatal("bit_alloc failed");
-        }
+        (*s).tabs = Some(BitStr::new(screen_size_x(s)));
 
         let mut i = 8;
         while i < screen_size_x(s) {
-            bit_set((*s).tabs, i);
+            (*s).tabs.as_mut().unwrap().bit_set(i);
             i += 8;
         }
     }

src/bitstr.rs
@@ -1,40 +1,38 @@
-pub type bitstr_t = u8;
-
-pub unsafe fn bit_alloc(nbits: u32) -> *mut u8 {
-    unsafe { libc::calloc(nbits.div_ceil(8) as usize, 1).cast() }
+#[repr(transparent)]
+pub struct BitStr {
+    bits: Box<[u8]>,
 }
 
-pub unsafe fn bit_set(bits: *mut u8, i: u32) {
-    unsafe {
-        let byte_index = i / 8;
-        let bit_index = i % 8;
-        *bits.add(byte_index as usize) |= 1 << bit_index;
-    }
-}
-
-#[inline]
-pub unsafe fn bit_clear(bits: *mut u8, i: u32) {
-    unsafe {
-        let byte_index = i / 8;
-        let bit_index = i % 8;
-        *bits.add(byte_index as usize) &= !(1 << bit_index);
-    }
-}
-
-/// clear bits start..=stop in bitstring
-pub unsafe fn bit_nclear(bits: *mut u8, start: u32, stop: u32) {
-    unsafe {
-        // TODO this is written inefficiently, assuming the compiler will optimize it. if it doesn't rewrite it
-        for i in start..=stop {
-            bit_clear(bits, i);
+impl BitStr {
+    pub fn new(nbits: u32) -> Self {
+        Self {
+            bits: vec![0; nbits.div_ceil(8) as usize].into_boxed_slice()
         }
     }
-}
 
-pub unsafe fn bit_test(bits: *const u8, i: u32) -> bool {
-    unsafe {
+    pub fn bit_set(&mut self, i: u32) {
         let byte_index = i / 8;
         let bit_index = i % 8;
-        (*bits.add(byte_index as usize) & (1 << bit_index)) != 0
+        self.bits[byte_index as usize] |= 1 << bit_index;
+    }
+
+    #[inline]
+    pub fn bit_clear(&mut self, i: u32) {
+        let byte_index = i / 8;
+        let bit_index = i % 8;
+        self.bits[byte_index as usize] &= !(1 << bit_index); 
+    }
+
+    pub fn bit_nclear(&mut self, start: u32, stop: u32) {
+        // TODO this is written inefficiently, assuming the compiler will optimize it. if it doesn't rewrite it
+        for i in start..=stop {
+            self.bit_clear(i);
+        }
+    }
+
+    pub fn bit_test(&self, i: u32) -> bool {
+        let byte_index = i / 8;
+        let bit_index = i % 8;
+        self.bits[byte_index as usize] & (1 << bit_index) != 0
     }
 }

commit 79802e574594bf53c419768f2df292a975d1bf24

fix crash when opening command completion menu
fixes crashed caused by double free introduced in
64684b08: refactor: reimplement bitstr safely

src/format.rs
@@ -971,7 +971,7 @@ pub unsafe fn format_cb_pane_tabs(ft: *mut format_tree) -> format_table_type {
 
         let mut first = true;
         for i in 0..(*(*wp).base.grid).sx {
-            if !(*wp).base.tabs.as_ref().unwrap().bit_test(i) {
+            if !(*wp).base.tabs.as_ref().unwrap().borrow().bit_test(i) {
                 continue;
             }
 

src/input.rs
@@ -1406,7 +1406,7 @@ unsafe fn input_c0_dispatch(ictx: *mut input_ctx) -> i32 {
                     // Don't tab beyond the end of the line.
                     // Find the next tab point, or use the last column if none.
                     (*s).cx += 1;
-                    if (*s).tabs.as_ref().unwrap().bit_test((*s).cx) {
+                    if (*s).tabs.as_ref().unwrap().borrow().bit_test((*s).cx) {
                         break;
                     }
                 }
@@ -1469,7 +1469,7 @@ unsafe fn input_esc_dispatch(ictx: *mut input_ctx) -> i32 {
             }
             Ok(input_esc_type::INPUT_ESC_HTS) => {
                 if (*s).cx < screen_size_x(s) {
-                    (*s).tabs.as_mut().unwrap().bit_set((*s).cx);
+                    (*s).tabs.as_ref().unwrap().borrow_mut().bit_set((*s).cx);
                 }
             }
             Ok(input_esc_type::INPUT_ESC_RI) => {
@@ -1544,7 +1544,7 @@ unsafe fn input_csi_dispatch(ictx: *mut input_ctx) -> i32 {
                         n -= 1;
                         loop {
                             cx -= 1;
-                            if cx == 0 || (*s).tabs.as_ref().unwrap().bit_test(cx) {
+                            if cx == 0 || (*s).tabs.as_ref().unwrap().borrow().bit_test(cx) {
                                 break;
                             }
                         }
@@ -1761,10 +1761,10 @@ unsafe fn input_csi_dispatch(ictx: *mut input_ctx) -> i32 {
                 -1 => (),
                 0 => {
                     if (*s).cx < screen_size_x(s) {
-                        (*s).tabs.as_mut().unwrap().bit_clear((*s).cx);
+                        (*s).tabs.as_ref().unwrap().borrow_mut().bit_clear((*s).cx);
                     }
                 }
-                3 => (*s).tabs.as_mut().unwrap().bit_nclear(0, screen_size_x(s) - 1),
+                3 => (*s).tabs.as_ref().unwrap().borrow_mut().bit_nclear(0, screen_size_x(s) - 1),
                 _ => log_debug!("{}: unknown '{}'", __func__, (*ictx).ch as u8 as char),
             },
             Ok(input_csi_type::INPUT_CSI_VPA) => {

src/lib.rs
@@ -132,6 +132,7 @@ use std::{
     },
     mem::{MaybeUninit, size_of, zeroed},
     ptr::{NonNull, addr_of, addr_of_mut, null, null_mut},
+    rc::Rc,
     sync::{
         Mutex,
         atomic::{self, AtomicBool, AtomicU32, AtomicU64},
@@ -1042,6 +1043,7 @@ enum screen_cursor_style {
 
 /// Virtual screen.
 #[repr(C)]
+#[derive(Clone)]
 struct screen {
     title: *mut u8,
     path: *mut u8,
@@ -1077,7 +1079,7 @@ struct screen {
     saved_cell: grid_cell,
     saved_flags: i32,
 
-    tabs: Option<BitStr>,
+    tabs: Option<Rc<RefCell<BitStr>>>,
     sel: *mut screen_sel,
 
     #[cfg(feature = "sixel")]

src/screen_.rs
@@ -165,11 +165,11 @@ pub unsafe fn screen_free(s: *mut screen) {
 /// Reset tabs to default, eight spaces apart.
 pub unsafe fn screen_reset_tabs(s: *mut screen) {
     unsafe {
-        (*s).tabs = Some(BitStr::new(screen_size_x(s)));
+        (*s).tabs = Some(Rc::new(RefCell::new(BitStr::new(screen_size_x(s)))));
 
         let mut i = 8;
         while i < screen_size_x(s) {
-            (*s).tabs.as_mut().unwrap().bit_set(i);
+            (*s).tabs.as_ref().unwrap().borrow_mut().bit_set(i);
             i += 8;
         }
     }

src/screen_redraw.rs
@@ -428,7 +428,7 @@ pub unsafe fn screen_redraw_make_pane_status(
         let mut px: u32;
         let mut py: u32;
         let mut ctx: MaybeUninit<screen_write_ctx> = MaybeUninit::uninit();
-        let mut old: MaybeUninit<screen> = MaybeUninit::uninit();
+        let mut old: screen;
         let pane_status = (*rctx).pane_status;
 
         let ft = format_create(
@@ -462,7 +462,7 @@ pub unsafe fn screen_redraw_make_pane_status(
             width = (*wp).sx - 4;
         }
 
-        memcpy__(old.as_mut_ptr(), &raw const (*wp).status_screen);
+        old = (*wp).status_screen.clone();
         screen_init(&raw mut (*wp).status_screen, width, 1, 0);
         (*wp).status_screen.mode = mode_flag::empty();
 
@@ -495,11 +495,11 @@ pub unsafe fn screen_redraw_make_pane_status(
         free_(expanded);
         format_free(ft);
 
-        if grid_compare((*wp).status_screen.grid, (*old.as_mut_ptr()).grid) == 0 {
-            screen_free(old.as_mut_ptr());
+        if grid_compare((*wp).status_screen.grid, old.grid) == 0 {
+            screen_free(&raw mut old);
             return 0;
         }
-        screen_free(old.as_mut_ptr());
+        screen_free(&raw mut old);
         1
     }
 }

src/status.rs
@@ -584,8 +584,6 @@ pub unsafe fn status_message_redraw(c: *mut client) -> i32 {
         let sl = &raw mut (*c).status;
         let mut ctx: screen_write_ctx = zeroed();
         let s = (*c).session;
-        let mut old_screen: screen = zeroed();
-
         // size_t len;
         // u_int lines, offset, messageline;
         let mut gc: grid_cell = zeroed();
@@ -594,7 +592,7 @@ pub unsafe fn status_message_redraw(c: *mut client) -> i32 {
         if (*c).tty.sx == 0 || (*c).tty.sy == 0 {
             return 0;
         }
-        memcpy__(&raw mut old_screen, (*sl).active);
+        let mut old_screen = (*(*sl).active).clone();
 
         let mut lines = status_line_size(c);
         if lines <= 1 {
@@ -805,18 +803,18 @@ pub unsafe fn status_prompt_redraw(c: *mut client) -> i32 {
         let mut ctx: screen_write_ctx = zeroed();
 
         let s = (*c).session;
-        let mut old_screen: screen = zeroed();
 
         let offset: u32;
 
         let mut gc: grid_cell = zeroed();
         let mut cursorgc: grid_cell = zeroed();
+        let mut old_screen: screen;
 
         'finished: {
             if (*c).tty.sx == 0 || (*c).tty.sy == 0 {
                 return 0;
             }
-            memcpy__(&raw mut old_screen, (*sl).active);
+            old_screen = (*(*sl).active).clone();
 
             let mut lines = status_line_size(c);
             if lines <= 1 {